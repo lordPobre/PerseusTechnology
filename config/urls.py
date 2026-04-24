efed9064f0b6fd31329c489f6b1568c91dfcdb43
Carlos Lopez <clopez.ti@vcchile.cl>
2026-04-24 18:30:56 -0400

    correccion

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('perseus-access-x1/', admin.site.urls),
     path('', include('core.urls')), 
 ]
