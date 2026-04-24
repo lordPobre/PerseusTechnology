b49e863fafb034b0c62482ccf3e809307db835fe
Carlos Lopez <clopez.ti@vcchile.cl>
2026-04-23 21:03:56 -0400

    correcciones

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 echo "--- INICIO DEL BUILD ---"
-
-# Usamos python3 y nos aseguramos de que pip esté actualizado
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-# Ejecutamos collectstatic usando python3
 python3 manage.py collectstatic --noinput --clear
-
 echo "--- FIN DEL BUILD ---"
